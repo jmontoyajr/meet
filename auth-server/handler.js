f3a0348266b368ed5ab2da2fb839561018121e72
jmontoyajr <82304971+jmontoyajr@users.noreply.github.com>
2021-12-29 13:45:29 -0800

    new edits to meet app

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,5 +1,4 @@
 const { google } = require("googleapis");
-const { resultingClientExists } = require("workbox-core/_private");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
 /**
@@ -23,7 +22,7 @@ const credentials = {
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   redirect_uris: ["https://jmontoyajr.github.io/meet/"],
-  javascript_origins: ["https://jmontoyajr.github.io", "http://localhost:3000"],
+  javascript_origins: ["https://jmontoyajr.github.io", "http://localhost:3000", "http://localhost:8080"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
@@ -116,7 +115,7 @@ module.exports.getCalendarEvents = async (event) => {
   const oAuth2Client = new google.auth.OAuth2(
     client_id,
     client_secret,
-    redirect_uris[0]
+    redirect_uris[0],
   );
   // Decode authorization code extracted from the URL query
   const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
@@ -127,12 +126,6 @@ module.exports.getCalendarEvents = async (event) => {
    *  Exchange authorization code for access token with a “callback” after the exchange,
    *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
    */
-    oAuth2Client.getToken(access_token, (err, token) => {
-      if (err) {
-        return reject(err);
-      }
-      return resolve(token);
-    });
 
     calendar.events.list(
       {
